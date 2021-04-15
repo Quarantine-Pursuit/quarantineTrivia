03b497dc18f5c62169bdab4777e89a53c095b29a
unknown <pjb9299@hotmail.com>
2021-04-15 15:01:46 -0400

    added pseudo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import './App.css';
+import {useState} from 'react';
+import axios from 'axios';
+
 
 function App() {
   return (
     <div className="App">
+      
     </div>
   );
 }
 
 export default App;
+
+// create function/onSubmit to get userInput from categories and # of questions
+// use the value from userInput to call API
+// use axios, fetch data from API.
+// store questions in to useState
+// Map through the questions array and display trivia
+// Use the type object from API to display either multiple choice or true or false
+// Display answer choices as radio?
+// create a button for user answer submit. Once answer submitted, disabled the form so they cant go back and fix their answer
+// Create a display, letting user know they are correct or wrong, and move on  to the next question (with set timeout probably).
+
+
+// Create a button that initially displays Saved Games
+// on click, use ternaries to toggle saved games and change the button display to Current Game
+// Inside saved game, create an onclick function on the chevron so that the user can load their saved games from database
+
+
